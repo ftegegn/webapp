f28453e7e52fe279b84b520ea79d4ee97cd4586b
ftegegn <fasikaicthd@gmail.com>
2024-08-25 15:08:29 +0300

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -5,14 +5,19 @@ DC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DC_PROJECT="dependency-check scan: $(pwd)"
 DATA_DIRECTORY="$DC_DIRECTORY/data"
 CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
+REPORT="$DC_DIRECTORY/reports"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $DATA_DIRECTORY"
-    mkdir -p "$DATA_DIRECTORY/reports"
+    mkdir -p "$DATA_DIRECTORY"
 fi
 if [ ! -d "$CACHE_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $CACHE_DIRECTORY"
     mkdir -p "$CACHE_DIRECTORY"
+fi
+if [ ! -d "$REPORT" ]; then
+    echo "Initially creating report directory: $REPORT"
+    mkdir -p "$REPORT"
 fi
 
 # Make sure we are using the latest version
@@ -28,6 +33,6 @@ docker run --rm \
     --scan /src \
     --format "ALL" \
     --project "$DC_PROJECT" \
-    --out "$DC_DIRECTORY/reports"
+    --out "$REPORT"
     # Use suppression like this: (where /src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
